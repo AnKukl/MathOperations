f71b584b7223115111a84e7ad175ba9032ed5cec
AnKukl <131239026+AnKukl@users.noreply.github.com>
2023-04-29 11:20:10 +0200

    Add files via upload

---
+++ T1.java
@@ -0,0 +1,21 @@
+public class T1 {
+    public static void main(String[] args) {
+        char letter = 'G';
+        System.out.println("char:" + letter);
+        int year = 89;
+        System.out.println("integer:" + year);
+        byte count = 4;
+        System.out.println("byte:" + count);
+        short balance = 56;
+        System.out.println("short:" + balance);
+        double f1 = 4.7333436;
+        System.out.println("float:" + f1);
+        double d1 = 4.355453532;
+        System.out.println("double:" + d1);
+        long bolshoi = 12121;
+        System.out.println("long:" + bolshoi);
+
+
+
+    }
+}
---
+++ T2.java
@@ -0,0 +1,11 @@
+public class T2 {
+    public static void main(String[] args) {
+        int n = 365;
+        int e = n % 10;
+        int d = n / 10 % 10;
+        int s = n / 100;
+        System.out.println(s);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
